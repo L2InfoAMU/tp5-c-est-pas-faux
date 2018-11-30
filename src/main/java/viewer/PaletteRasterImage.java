0e8b55a7a19ff63f7c8d00c2a8b021fa9c6f6505
Damien <damien.marrou@etu.univ-amu.fr>
2018-11-30 09:56:19 +0100

    création des set et get de Heigth et widht

--- src/main/java/viewer/PaletteRasterImage.java
+++ src/main/java/viewer/PaletteRasterImage.java
@@ -4,48 +4,63 @@ import image.Image;
 import image.Pixel;
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class PaletteRasterImage implements Image {
 
     public int width, height;
-    public PaletteRasterImage palette;
+    List<Color> palette;
+    int[][] indexesOfColors;
 
-    public PaletteRasterImage(Color color, int width, int height) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                new Pixel(i, j, color);
 
-            }
-        }
+    public PaletteRasterImage(int width, int height, List<Color> palette) {
+        this.width = width;
+        this.height = height;
+        this.palette = palette;
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public Color getPixelColor(int x, int y) {
-        return getPixelColor(x,y);
+    public PaletteRasterImage(int[][] indexesOfColors) {
+        this.indexesOfColors = indexesOfColors;
     }
+
+    public void createRepresentation(){
+        this.indexesOfColors=new int[height][width];
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+
+    }
+
+    public Color getPixelColor(int x, int y){
+
+    }
+
+    public void setPixelsColor(Color[][] pixels){
+
+    }
+
+    private void setPixelsColor(Color color){
+
+    }
+
 
     @Override
     public int getWidth() {
-        return getWidth();
+        return this.width;
     }
 
     @Override
     public int getHeight() {
-        return getHeight();
+        return this.height;
+    }
+
+    protected void setWidth(int width){
+        this.width=width;
+    }
+
+    protected void setHeight(int height){
+        this.height=height;
     }
 }
 
