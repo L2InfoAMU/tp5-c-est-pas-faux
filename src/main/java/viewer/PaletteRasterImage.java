a052dc218a94b448c466a2cb1de3a73ca61193e5
lucas <lucas_fernandez@orange.fr>
2018-11-30 11:53:26 +0100

    Merge branch 'master' of /Users/lucas/Documents/Licence 2/Semestre 1/Programmation 2/tp5-c-est-pas-faux with conflicts.

--- src/main/java/viewer/PaletteRasterImage.java
+++ src/main/java/viewer/PaletteRasterImage.java
@@ -2,79 +2,50 @@ package viewer;
 
 import image.Image;
 import image.Pixel;
-import static util.Matrices.*;
 import javafx.scene.paint.Color;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PaletteRasterImage implements Image {
 
     public int width, height;
-    List<Color> palette;
-    int[][] indexesOfColors;
+    public PaletteRasterImage palette;
 
+    public PaletteRasterImage(Color color, int width, int height) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                new Pixel(i, j, color);
 
-    public PaletteRasterImage(int width, int height, List<Color> palette) {
-        this.width = width;
-        this.height = height;
-        this.palette = palette;
-    }
-
-
-    public PaletteRasterImage(int[][] indexesOfColors) {
-        this.indexesOfColors = indexesOfColors;
-    }
-
-
-    public void createRepresentation(){
-        this.indexesOfColors=new int[height][width];
-        this.palette=new ArrayList<>();
-    }
-
-    public void setPixelColor(Color color, int x, int y){
-        int index=palette.indexOf(color);
-        if(index==-1){
-            palette.add(color);
-            index=palette.size()-1;
-        }
-        indexesOfColors[y][x]=index;
-    }
-
-    public Color getPixelColor(int x, int y){
-        return palette.get(indexesOfColors[y][x]);
-    }
-
-    public void setPixelsColor(Color[][] pixels){
-
-    }
-
-    private void setPixelsColor(Color color){
-       this.palette.clear();
-       palette.add(color);
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                indexesOfColors[j][i]=0;
             }
         }
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return getPixelColor(x,y);
+    }
+
     @Override
     public int getWidth() {
-        return this.width;
+        return getWidth();
     }
 
     @Override
     public int getHeight() {
-        return this.height;
-    }
-
-    protected void setWidth(int width){
-        this.width=width;
-    }
-
-    protected void setHeight(int height){
-        this.height=height;
+        return getHeight();
     }
 }
 
